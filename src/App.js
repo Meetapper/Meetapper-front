1e2c02374ef972cba213eebe4e1997d9f68f88cc
Jakub Solecki <jakub.solecki99@gmail.com>
2022-04-02 21:53:14 +0200

    basic auth works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
+import Register from './pages/Register';
+import Logout from './pages/Logout'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/logout" element={<Logout />}/>
       </Routes>
     </Router>
   );
