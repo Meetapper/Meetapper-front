57afe0ab7cd6ccf74dd387f6f4d145e245b219a3
Adam Berezka <adam.berezka99@gmail.com>
2022-04-03 10:53:24 +0200

    chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,9 @@ import { Paper, TextField, Box, Button } from "@mui/material";
 import React, { useState } from "react"
 import SendIcon from '@mui/icons-material/Send';
 import ChatMessage from "./ChatMessage";
+import {db} from "../Firebase";
+import {useLocation} from "react-router-dom";
+import {collection, doc, getDocs, onSnapshot} from "firebase/firestore"
 
 const mockMessages = [
     {
@@ -15,6 +18,7 @@ const mockMessages = [
 ];
 
 const Chat = () => {
+    const eventId = useLocation().state.id;
     const [message, setMessage] = useState('');
     const sendMessage = () => {
         setChatMessages([ ...chatMessages, {
@@ -25,11 +29,31 @@ const Chat = () => {
     }
     const [chatMessages, setChatMessages] = useState(mockMessages);
 
+    getDocs(collection(db, "meetings")).then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(doc.data())
+            //if(doc.data().id === eventId) {
+            setChatMessages(doc.data().message);
+            console.log("MESSAGES SET")
+            //}
+        });
+    });
+
+    // const unsubscribe = onSnapshot(doc(db, "meetings", eventId), (querySnapshot) => {
+    //     let messages = [];
+    //     querySnapshot.forEach((doc) => {
+    //         if(doc.data().id === eventId)
+    //             messages = doc.data().message;
+    //     });
+    //     setChatMessages(messages);
+    //     console.log(messages);
+    // });
+
     return (
         <Paper sx={{ height: "580px", marginTop: "15px" }} elevation={3}>
             <Box display="flex" flexDirection="column" sx={{ height: "100%" }}>
                 <Box flexGrow={2} sx={{padding: "10px"}}>
-                    {chatMessages.map(message => <ChatMessage message={message} />)}
+                    {chatMessages.map((message, index) => <ChatMessage key={String.valueOf(index * 5)} message={message} />)}
                 </Box>
                 <Box display="flex" justifyContent="center" alignContent="center">
                     <Box flexGrow={2}>
